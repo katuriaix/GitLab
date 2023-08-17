cbcee8bdaa67148edf278b5b4229cdd879915baa
Sreenu <katuriaix4@gmail.com>
2023-08-17 15:08:35 +0530

    I added new shell scrip testing

---
+++ 06variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is called array, instead of single value it can hold multiople values
+
+PERSON=("katuri" "sreeni" "Rao")
+
+# inside array we always refer values with inside ans it starts from 0
+
+echo "First person: ${PERSON[0]}"
+
+#echo "All persons: ${PERSON[@]}"
