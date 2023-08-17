966de9c75753f42243170ada81b24c79ae5f1ea3
Sreenu <katuriaix4@gmail.com>
2023-08-17 14:24:10 +0530

    I added new shell scrip testing

---
+++ 03variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello world, I am learning DevOps course"
+
+PERSON1=$1 
+PERSON2=$2
+
+echo "$PERSON1 : Hi $PERSON2, Good Morning"
+echo "$PERSON2 : Good Morning $PERSON1"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : Good and you $PERSON1"
