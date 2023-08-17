0cfe2905db5c6ea940e215b2abd6a0e96e200c58
Sreenu <katuriaix4@gmail.com>
2023-08-17 14:57:33 +0530

    I added new shell scrip testing

--- 05variable.sh
+++ 05variable.sh
@@ -5,3 +5,9 @@ echo "Please enter your username"
 read USERNAME  # this is the variable where the value of the one user enter
 
 echo "User name entered is: $USERNAME"
+
+echo "Please enter your Password"
+
+read PASSWORD
+
+echo "User name entered is: $PASSWORD"
