31db3583612d31c2a8d1158f1cfc1608bca6b3f0
Sreenu <katuriaix4@gmail.com>
2023-08-18 14:43:28 +0530

    new file created for testing purpose

--- 05variable.sh
+++ 05variable.sh
@@ -2,12 +2,16 @@
 
 echo "Please enter your username"
 
-read -s USERNAME  # this is the variable where the value of the one user enter
+read  USERNAME  # this is the variable where the value of the one user enter
 
 echo "User name entered is: $USERNAME"
 
 echo "Please enter your Password"
 
-read -s PASSWORD
+read PASSWORD
 
 echo "User name entered is: $PASSWORD"
+
+read EMAILID
+
+echo "enter your email id: $EMAILID"
