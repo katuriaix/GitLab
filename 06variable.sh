3c0bcac895722f1295593f911296ff6aecbba746
Sreenu <katuriaix4@gmail.com>
2023-08-18 15:14:57 +0530

    new file created for testing purpose

--- 06variable.sh
+++ 06variable.sh
@@ -2,7 +2,7 @@
 
 # This is called array, instead of single value it can hold multiople values
 
-PERSON=("katuri" "sreeni" "Rao")
+PERSON=("katuri" "12" "sreeni" "Rao")
 
 # inside array we always refer values with inside ans it starts from 0
 
@@ -10,4 +10,4 @@ echo "First person: ${PERSON[0]}"
 
 echo "All persons: ${PERSON[@]}"
 
-echo "Total count: ${PERSON[$#]}"
+#echo "Total count: ${PERSON[$#]}"
