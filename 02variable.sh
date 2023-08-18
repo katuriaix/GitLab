920a3d93c4b7b71f140c002538341aa5d1d7a804
Sreenu <katuriaix4@gmail.com>
2023-08-18 14:19:20 +0530

    new file created for testing purpose

--- 02variable.sh
+++ 02variable.sh
@@ -2,6 +2,6 @@
 
 # shell script run the commands inside $() and stores the out put into variable called DATE.
 
-DATE=$(date +%m/%d/%y%T)
+DATE=$(date +%m/%d/%y%T%r)
 #$date --date="yesterday"
 echo "Hello, this script is executed at timestamp:$DATE"
